bd8ddf87cd962396e2fc262e894a002c36a075a7
ahaha <101982272+Git4RJS@users.noreply.github.com>
2023-06-18 13:32:45 +0700

    Update MyRecord.js

--- client/components/MyRecord.js
+++ client/components/MyRecord.js
@@ -1,1 +1,128 @@
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableHighlight,
+  View,
+} from 'react-native';
+
+import { SwipeListView } from 'react-native-swipe-list-view';
+
+export default function Basic() {
+  const [listData, setListData] = useState(
+    Array(20)
+      .fill('')
+      .map((_, i) => ({ key: `${i}`, text: `item #${i}` }))
+  );
+
+  const closeRow = (rowMap, rowKey) => {
+    if (rowMap[rowKey]) {
+      rowMap[rowKey].closeRow();
+    }
+  };
+
+  const onRowDidOpen = (rowKey) => {
+    console.log('This row opened', rowKey);
+  };
+
+  const renderItem = (data) => (
+    <TouchableHighlight
+      onPress={() => console.log('You touched me')}
+      style={styles.rowFront}
+      underlayColor={'#AAA'}>
+      <View>
+        <Text style={styles.txt}>File name: {data.item.text}</Text>
+        <Text style={styles.txt}>ID Uploader: {data.item.text}</Text>
+      </View>
+    </TouchableHighlight>
+  );
+
+  const renderHiddenItem = (data, rowMap) => (
+    <View style={styles.rowBack}>
+      <TouchableOpacity
+        style={[styles.backRightBtn, styles.backRightBtnLeft]}
+        onPress={() => deleteRow(rowMap, data.item.key)}>
+        <Text style={styles.backTextWhite}>Download</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.backRightBtn, styles.backRightBtnRight]}
+        onPress={() => closeRow(rowMap, data.item.key)}>
+        <Text style={styles.backTextWhite}>Cancel</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+ 
+      <SwipeListView
+        data={listData}
+        renderItem={renderItem}
+        renderHiddenItem={renderHiddenItem}
+        leftOpenValue={75}
+        rightOpenValue={-150}
+        previewRowKey={'0'}
+        previewOpenValue={-40}
+        previewOpenDelay={3000}
+        onRowDidOpen={onRowDidOpen}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  backTextWhite: {
+    color: '#FFF',
+  },
+  txt: {
+    color: '#0039a6',
+    fontSize: 15,
+  },
+  rowFront: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: '#0039a6',
+    justifyContent: 'center',
+    height: 80,
+  },
+    title: {
+    marginTop: 60,
+    marginBottom: 40,
+    textAlign: 'center',
+    fontSize: 35,
+    color: '#0039a6',
+  },
+
+  rowBack: {
+    alignItems: 'center',
+
+    flex: 1,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 15,
+  },
+  backRightBtn: {
+    alignItems: 'center',
+    bottom: 0,
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    width: 75,
+  },
+  backRightBtnLeft: {
+    backgroundColor: '#03C03C',
+    right: 75,
+  },
+  backRightBtnRight: {
+    backgroundColor: 'red',
+    right: 0,
+  },
+});
 
